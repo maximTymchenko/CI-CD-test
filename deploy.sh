1f905e37f944885c51b314f1408e8560a4456f4f
maximTymchenko <maximTymchenko>
2021-01-02 20:29:15 +0200

    fuck this shit

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-aws s3 rm --recursive s3://ci-test1/index.html
-aws s3 cp public s3://ci-test1/index.html --recursive --exclude "*.DS_Store" --acl public-read
-aws cloudfront create-invalidation --distribution-id E1CJEH343DGUK3 --paths "/*"
